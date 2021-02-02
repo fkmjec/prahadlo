cdfca94bcf3aa168386e26510c79fdbc7ac678ca
František Kmječ <frantisek.kmjec@gbl.cz>
2021-02-02 11:09:06 +0100

    Add more testing support

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 mod model;
 
 use std::path::Path;
+use std::io;
 
-use crate::model::data_structures::Node;
 use crate::model::loader;
 
 fn main() {
     println!("Hello, world! Prahadlo here!");
     let graph = loader::load_transport_network(Path::new("data/"));
-    println!("{}", graph.find_connection("U675S1", "U115S1", 1100).unwrap().unwrap());
+    loop {
+        let mut input = String::new();
+        println!("Please provide the two stop IDs separated by space!");
+        io::stdin().read_line(&mut input).expect("Unable to read input!");
+        let split: Vec<&str> = input.split(" ").collect();
+
+        println!("{}", graph.find_connection(split[0], split[1], 1100).unwrap().unwrap());
+    }
 }
